020ecfa5533faa9edc50a3120b61dcdb3f812997
zhchi <zhchiearth@gmail.com>
2024-03-29 17:05:42 +0800

    update

--- src/lib/Position.sol
+++ src/lib/Position.sol
@@ -3,18 +3,15 @@ pragma solidity ^0.8.14;
 
 library Position {
     struct Info {
-        uint128 liquidity;
+        uint128 liquidity; // 记录的是，用户在这tick区间 的流动性
     }
 
-    function get(
-        mapping(bytes32 => Info) storage self,
-        address owner,
-        int24 lowerTick,
-        int24 upperTick
-    ) internal view returns (Position.Info storage position) {
-        position = self[
-            keccak256(abi.encodePacked(owner, lowerTick, upperTick))
-        ];
+    function get(mapping(bytes32 => Info) storage self, address owner, int24 lowerTick, int24 upperTick)
+        internal
+        view
+        returns (Position.Info storage position)
+    {
+        position = self[keccak256(abi.encodePacked(owner, lowerTick, upperTick))];
     }
 
     function update(Info storage self, uint128 liquidityDelta) internal {
--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -4,14 +4,10 @@ pragma solidity ^0.8.14;
 library Tick {
     struct Info {
         bool initialized;
-        uint128 liquidity;
+        uint128 liquidity; // 记录这个区间的流动性
     }
 
-    function update(
-        mapping(int24 => Tick.Info) storage self,
-        int24 tick,
-        uint128 liquidityDelta
-    ) internal {
+    function update(mapping(int24 => Tick.Info) storage self, int24 tick, uint128 liquidityDelta) internal {
         Tick.Info storage tickInfo = self[tick];
         uint128 liquidityBefore = tickInfo.liquidity;
         uint128 liquidityAfter = liquidityBefore + liquidityDelta;
